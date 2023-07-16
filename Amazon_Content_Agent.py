f731464d86ca00fa5ec7af4c74e55e7d9da286d1
Leandro Omar Garcia <54212751+leandroomargarcia@users.noreply.github.com>
2023-07-16 00:50:36 -0300

    Update Amazon_Content_Agent.py

--- Amazon_Content_Agent.py
+++ Amazon_Content_Agent.py
@@ -118,11 +118,7 @@ def main():
         )
         prompt_response = response["choices"][0]["message"]['content'].strip()
         request = 'Description Product: ' + prompt_response
-        #st.text(request)
-        # Primer reservas un espacio en pantalla con st.empty()
-        placeholder = st.empty()
-        # Más adelante en tu código, puedes usar esa referencia para llenar el espacio reservado
-        placeholder.text(request)
+        st.text(request)
     
 if __name__ == '__main__':
     main()
